2c9d9050d4385be94a86078e9d6c603ce702c019
AustinMinnerup <austin_minnerup@yahoo.com>
2016-08-18 17:42:13 -0500

    mid of the letters
    this program gets the mid of 2 letters that you the user types in and it
    gives you the mid letter of them two

---
+++ question 7/Source.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+int main()
+{
+	char letter1, letter2,letter3;
+	std::cin >> letter1 >> letter2;
+	letter3 = (letter1+letter2)/2;
+	printf("%c", letter3);
+	system("pause");
+
+}
